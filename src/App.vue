96780a59689ac32b32de42a8556927e27ce29648
Galileo <kevingalileo100@gmail.com>
2022-06-07 01:01:53 -0500

    Quick Links

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@
 
     <!-- BODY -->
     <main class="container mt-3">
+      <actions-nav></actions-nav>
       <main-layout>
         <template v-slot:content> <router-view /> </template>
         <template v-slot:sidebarRigth> <c-events /> </template>
@@ -24,9 +25,10 @@ import LayoutHeader from "@/components/headers/LayoutHeader.vue";
 import MainLayout from "@/views/MainLayout.vue";
 import cFooter1 from "./components/footers/cFooter1.vue";
 import CEvents from "./components/cEvents.vue";
+import ActionsNav from "./components/navs/ActionsNav.vue";
 
 export default {
-  components: { LayoutHeader, MainLayout, cFooter1, CEvents },
+  components: { LayoutHeader, MainLayout, cFooter1, CEvents, ActionsNav },
 };
 </script>
 <style lang="scss">
